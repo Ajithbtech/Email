6e2c27b5ec748e9e89b85e97f6fa0ad442d73e4b
Ajithbtech <ajithmsp722@gmail.com>
2022-04-02 21:35:35 -0700

    first commit

---
+++ src/main/java/com/mkyong/Application.java
@@ -0,0 +1,78 @@
+package com.mkyong;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+//import javax.mail.MessagingException;
+//import javax.mail.internet.MimeMessage;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    //https://docs.spring.io/spring/docs/5.1.6.RELEASE/spring-framework-reference/integration.html#mail
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+
+        System.out.println("Sending Email...");
+
+        try {
+
+            sendEmailWithAttachment();
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Done");
+
+    }
+
+
+
+    void sendEmailWithAttachment() throws MessagingException, IOException {
+
+        MimeMessage msg = javaMailSender.createMimeMessage();
+
+        // true = multipart message
+        MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+        helper.setTo("ak57tech@gmail.com");
+
+        helper.setSubject("Testing from Spring Boot");
+
+        // default = text/plain
+        //helper.setText("Check attachment for image!");
+
+        // true = text/html
+        helper.setText("<h1>testing!</h1>", true);
+
+        //FileSystemResource file = new FileSystemResource(new File("classpath:android.png"));
+
+        //Resource resource = new ClassPathResource("android.png");
+        //InputStream input = resource.getInputStream();
+
+        //ResourceUtils.getFile("classpath:android.png");
+
+//        helper.addAttachment("my_photo.png", new ClassPathResource("android.png"));
+
+        javaMailSender.send(msg);
+
+    }
+}
